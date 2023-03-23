a18b370c3ea17a99ed1ffd567a233ab6e2225de2
Philemon04 <philmuthomi04@gmail.com>
2023-03-23 00:35:22 -0700

    Some minor changes

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,9 +16,13 @@ int largest_number(int a, int b, int c)
     {
         largest = a;
     }
-    else if (b > c)
+    else if (b > a && a > c)
     {
         largest = b;
+    }
+    else if (c > a && a > b)
+    {
+	largest = c;
     }
     else
     {
