52346662cec2a12a97581c00c68ceddf960c415d
Philemon04 <philmuthomi04@gmail.com>
2023-04-05 06:46:41 -0700

    Some minor changes

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,29 +1,29 @@
 #include "main.h"
 
 /**
- * check - Checks to see if number is prime
+ * check - checks for the square root
  * @a:int
  * @b:int
- * Return:int
+ *
+ * Return: int
  */
 int check(int a, int b)
 {
-	if (b < 2 || b % a == 0)
-		return (0);
-	else if (a > b / 2)
-		return (1);
-	else
-		return (check(a + 1, b));
+	if (a * a == b)
+		return (a);
+	if (a * a > b)
+		return (-1);
+	return (check(a + 1, b));
 }
 
 /**
- * is_prime_number - States if number is prime
- * @n:int
- * Return:int
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer to find sqrt of
+ * Return: natural square root or -1
  */
-int is_prime_number(int n)
+int _sqrt_recursion(int n)
 {
-	if (n == 2)
-		return (1);
-	return (check(2, n));
+	if (n == 0)
+		return (0);
+	return (check(1, n));
 }
